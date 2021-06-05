3b1edd444e45c1377d2bdb790603431bc836e9c5
Mewni Lasanya Alahakoon <56906579+Mewni@users.noreply.github.com>
2021-06-05 14:12:59 +0530

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -11,9 +11,37 @@ sign_in_btn.addEventListener("click", () => {
   container.classList.remove("sign-up-mode");
 });
 */
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    var uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
+
+function signup() {
+  let email = document.getElementById('email').value
+  let Password = document.getElementById('password').value
+
+  firebase.auth().createUserWithEmailAndPassword(email, Password)
+}
+
+
  function login() {
    var userEmail = document.getElementById("email_feild").value;
    var userPass = document.getElementById("password_feild").value;
 
-   window.alert(userEmail + "" +userPass);
+   //Sign In User with Email and Password
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    
+    window.alert("Error : " + errorMessage);
+});
+    
  }
