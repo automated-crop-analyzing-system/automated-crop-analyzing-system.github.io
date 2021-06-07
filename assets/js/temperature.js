27f75245896e10f329defeb5fb063c735657e18e
Mewni Lasanya Alahakoon <56906579+Mewni@users.noreply.github.com>
2021-06-07 11:29:12 +0530

    Update files

--- assets/js/temperature.js
+++ assets/js/temperature.js
@@ -1,4 +1,10 @@
-$(document).ready(function() {
-    var rootRef = firebase.database().ref().child("")
+var rootRef = firebase.database().ref().child("report");
 
-});
+    rootRef.on("child_added", snap => {
+        var name = snap.child("Date").val();
+        var email = snap.child("Temperature").val();
+
+        $("#table_body").append("<tr><td>" + date +"</td><td>"+ temperature +"</td></tr>");
+
+    });
+
